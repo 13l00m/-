441d36a35789ea74c11bca8b41cc8a7306f55892
13l00m <58256808+13l00m@users.noreply.github.com>
2022-08-14 16:24:30 +0800

    Update main.py

--- LocalMdImageTransTRemote/main.py
+++ LocalMdImageTransTRemote/main.py
@@ -2,5 +2,5 @@
 
 import lib.ParseMarkdown
 if __name__=="__main__":
-    FilePath = "D:\\chromedownload\\xmlDecoder反序列化\\xmlDecoder反序列化\\Java\\基础&分析\\分析\\中间件组件\\apache\\cb链\\xmlDecoder反序列化.md"
+    FilePath = "xx.md"
     lib.ParseMarkdown.LocalImgTransToRemote(FilePath)
